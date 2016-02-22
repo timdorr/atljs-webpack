c2b2b74d70222c4fd308a7f42415162b34957a0e
Tim Dorr <timdorr@timdorr.com>
2016-02-21 23:30:10 -0500

    :scissors:

--- 5/webpack.config.js
+++ 5/webpack.config.js
@@ -1,24 +1,10 @@
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
-
   entry: "./entry.js",
 
   output: {
       path: __dirname,
       filename: "bundle.js"
-  },
-
-  module: {
-    loaders: [
-      { loader: 'babel', test: /.js$/, include: __dirname }
-    ]
-  },
-
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
-    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
-  ]
+  }
 };
